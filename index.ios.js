1602a8fd3de7e015e4b493408243c27f72909b09
Neo <nihgwu@live.com>
2016-05-21 18:14:36 +0800

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,11 @@
-
-import React,{
+import React from 'react';
+import {
   View,
-  Component,
   ScrollView,
   Image
 } from 'react-native';
 
-export default class ImageViewZoom extends Component {
+export default class ImageViewZoom extends React.Component {
   render() {
     return (
       <View style={{flex:1}}>
